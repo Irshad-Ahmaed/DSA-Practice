c67258eb5df02e254334a630dd773175200af4fe
IRSHAD AHMAD <129326052+Irshad-Ahmaed@users.noreply.github.com>
2024-10-07 11:17:50 +0530

    Accenture Highest-Product

--- Arrays Hard/src/Merge_Overlap_Interval.java
+++ Arrays Hard/src/Merge_Overlap_Interval.java
@@ -2,27 +2,73 @@ import java.util.*;
 
 public class Merge_Overlap_Interval {
     public static void main(String[] args) {
-        int[][] arr = {{1,3},{1,2},{3,6},{8,9},{9,11}};
-        int n= arr.length;
-        List<List<Integer>> temp = new ArrayList<>();
+        int[][] arr = {{1, 3}, {1, 2}, {3, 6}, {8, 9}, {9, 11}};
+        int n = arr.length;
 
+        Arrays.sort(arr, new Comparator<int[]>() {
+            public int compare(int[] a, int[] b) {
+                return a[0] - b[0];
+            }
+        });
 
-        for (int i=0; i<n; i++){
-            for (int j=0; j<1; j++){
-                if (temp.isEmpty()){
-                    temp.add(Arrays.asList(arr[i][j], arr[i][j+1]));
-                }
-                else if (temp.getLast().get(1) < arr[i][j]) {
-                    temp.add(Arrays.asList(arr[i][j], arr[i][j+1]));
-                }
-                else if (temp.getLast().get(1) < arr[i][j+1]) {
-                    List<Integer> replace = new ArrayList<>();
-                    replace.add(temp.getLast().get(0));
-                    replace.add(arr[i][j+1]);
-                    temp.set(temp.indexOf(temp.getLast()), replace);
-                }
+//          Brute Force:
+//        List<List<Integer>> temp = new ArrayList<>();
+
+//        for (int i=0; i<n; i++){
+//            if (temp.isEmpty()){
+//                    temp.add(Arrays.asList(arr[i][0], arr[i][1]));
+//                    continue;
+//            }
+//            for (int j=0; j<1; j++){
+//                if (temp.getLast().get(1) < arr[i][j]) {
+//                    temp.add(Arrays.asList(arr[i][j], arr[i][j+1]));
+//                }
+//                else if (temp.getLast().get(1) < arr[i][j+1]) {
+//                    List<Integer> replace = new ArrayList<>();
+//                    replace.add(temp.getLast().get(0));
+//                    replace.add(arr[i][j+1]);
+//                    temp.set(temp.indexOf(temp.getLast()), replace);
+//                }
+//            }
+//        }
+//        System.out.println(temp);
+
+//        Optimal Approach:
+//        List<List<Integer>> temp = new ArrayList<>();
+
+//        for (int i=0; i<n; i++){
+//            if (temp.isEmpty() || temp.get(temp.size() - 1).get(1) < arr[i][0]){
+//                temp.add(Arrays.asList(arr[i][0], arr[i][1]));
+//            }
+//            else if (temp.get(temp.size() - 1).get(1) < arr[i][1]) {
+//                temp.get(temp.size()-1).set(1, arr[i][1]);
+//            }
+//        }
+//
+//        System.out.println(temp);
+
+//        Arrays.sort(arr, (a, b) -> a[0] - b[0]);
+        List<int[]> temp = new ArrayList<>();
+
+        if (arr.length == 0 || arr == null){
+            temp.toArray(new int[0][]);
+            System.out.println("exit");
+        }
+
+        int start = arr[0][0];
+        int end = arr[0][1];
+
+        for(int i=0; i<n; i++) {
+            if(arr[i][0] <= end) {
+                end = Math.max(end, arr[i][1]);
+            }
+            else {
+                temp.add(new int[]{start, end});
+                start = arr[i][0];
+                end = arr[i][1];
             }
         }
+        temp.add(new int[]{start, end});
         System.out.println(temp);
 
 
---
+++ highest_product.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class highest_product {
+    public static void main(String[] args) {
+        System.out.print("Assign the value of k: ");
+        Scanner sc = new Scanner(System.in);
+        int k = sc.nextInt();
+        int maxProduct = 1;
+
+        if (k<=3){
+            System.out.println("K is: " + k);
+        }
+
+        while(k > 4){
+            maxProduct *= 3;
+            k -= 3;
+        }
+
+        maxProduct *= k;
+
+        System.out.println("highest product value of k is: " + maxProduct);
+    }
+}
