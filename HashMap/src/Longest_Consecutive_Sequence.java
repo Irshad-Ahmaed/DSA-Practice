84e3be61d998ed996a8c4cecda3cc3a2614850c7
IRSHAD AHMAD <129326052+Irshad-Ahmaed@users.noreply.github.com>
2024-11-14 22:53:25 +0530

    Longest_Consecutive_Sequence_Answer

--- HashMap/src/Longest_Consecutive_Sequence.java
+++ HashMap/src/Longest_Consecutive_Sequence.java
@@ -1,5 +1,37 @@
+import java.util.HashMap;
+
 public class Longest_Consecutive_Sequence {
     public static void main(String[] args) {
+        int[] arr = {10,5,9,1,11,8,6,15,3,12,2};
 
+        HashMap<Integer, Boolean> mpp = new HashMap<>();
+
+//        First assume all elements are consecutive
+        for (int val: arr){
+            mpp.put(val, true);
+        }
+
+//        Now check if element are really consecutive, if they have their previous value in map
+        for (int val: arr){
+            if (mpp.containsKey(val-1)){
+                mpp.put(val, false);
+            }
+        }
+
+        int maxLen = 0;
+
+        for (int i=0; i< arr.length; i++){
+//        Only checking for element which are true in map
+            if (mpp.get(arr[i])){
+                int len = 1;
+                System.out.println(arr[i]);
+                while (mpp.containsKey(arr[i] + len)){
+                    len++;
+                }
+                if (len>maxLen) maxLen = len;
+            }
+        }
+
+        System.out.println("maxLen " + maxLen);
     }
 }
